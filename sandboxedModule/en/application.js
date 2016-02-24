08975088e35e02c0f4aea4362ecb3a5316c75db6
it <it>
2016-02-24 14:11:10 +0200

    Few tasks completed

--- sandboxedModule/en/application.js
+++ sandboxedModule/en/application.js
@@ -1,11 +1,14 @@
+'use strict';
 // File contains a small piece of the source to demonstrate main module
 // of a sample application to be executed in the sandboxed context by
 // another pice of code from `framework.js`. Read README.md for tasks.
 
 // Print from the global context of application module
 console.log('From application global context');
+setTimeout(() => {console.log('Timeout')}, 0);
+console.log(util.isRegExp(/hello/i));
 
 module.exports = function() {
-	// Print from the exported function context
+  // Print from the exported function context
   console.log('From application exported function');
 };
--- sandboxedModule/en/framework.js
+++ sandboxedModule/en/framework.js
@@ -1,19 +1,45 @@
+'use strict';
 // Example showing us how the framework creates an environment (sandbox) for
 // appication runtime, load an application code and passes a sandbox into app
 // as a global context and receives exported application interface
 
 // The framework can require core libraries
 var fs = require('fs'),
-    vm = require('vm');
+    vm = require('vm'),
+    util = require('util');
+
+var passedArg = process.argv.length > 2? process.argv.pop() : './application.js';
+var appName = passedArg;  //TODO change
+var fileName = passedArg; //TODO change
+if (!fileName.endsWith('.js')) {
+  fileName = fileName + '.js';
+}
 
 // Create a hash and turn it into the sandboxed context which will be
 // the global context of an application
-var context = { module: {}, console: console };
+var context = { 
+  module: {}, 
+  console: console, 
+  setTimeout: (callback, timeout) => { 
+    console.log('Set timeout called!'); 
+    setTimeout(callback, timeout); 
+  }, 
+  setInterval: setInterval,
+  util: util
+};
+context.console.log = function (data) {
+  var curDate = new Date().toISOString().
+    replace(/T/, ' ').       // replace T with a space
+    replace(/\..+/, '');     // delete the dot and everything after
+
+  console.log(fileName + curDate + data, arguments.slice(1));
+};
 context.global = context;
 var sandbox = vm.createContext(context);
 
+
 // Read an application source code from the file
-var fileName = './application.js';
+
 fs.readFile(fileName, function(err, src) {
   // We need to handle errors here
   
